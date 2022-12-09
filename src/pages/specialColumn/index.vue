86cff333b4ec55e417e5300f1c8fcd2211826e40
yangjianbin <1372502168@qq.com>
2022-12-09 17:44:42 +0800

    fix: 添加tabBar

---
+++ src/pages/specialColumn/index.vue
@@ -0,0 +1,38 @@
+<!--  -->
+<template>
+  <div class="wrapper">
+    
+		<uni-file-picker 
+			v-model="imageValue" 
+			fileMediatype="image" 
+			mode="grid" 
+			@select="select" 
+			@progress="progress" 
+			@success="success" 
+			@fail="fail" 
+		/>
+  </div>
+</template>
+
+<script>
+import uniFilePicker from "@/uni_modules/uni-file-picker/components/uni-file-picker/uni-file-picker";
+export default {
+  components: {
+    uniFilePicker
+  },
+
+  data () {
+    return {
+    }
+  },
+
+  computed: {},
+
+  methods: {},
+
+  created() {}
+}
+
+</script>
+<style lang='scss' scoped>
+</style>
---
+++ src/pages/user/index.vue
@@ -0,0 +1,24 @@
+<!--  -->
+<template>
+  <div class="wrapper">w de </div>
+</template>
+
+<script>
+export default {
+  components: {},
+
+  data () {
+    return {
+    }
+  },
+
+  computed: {},
+
+  methods: {},
+
+  created() {}
+}
+
+</script>
+<style lang='scss' scoped>
+</style>
