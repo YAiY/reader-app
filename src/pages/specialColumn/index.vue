b8edf54e552d5f45bb8525645e79ebc8a2afba83
yangjianbin <1372502168@qq.com>
2022-12-13 10:36:34 +0800

    fix: 小程序读取txt文本

--- src/pages/specialColumn/index.vue
+++ src/pages/specialColumn/index.vue
@@ -12,6 +12,10 @@
       file-extname="txt,pdf,word"
       @select="select"
 		/>
+    <button @click="selectFile">按钮</button>
+    <!-- <view>{{filePath}}</view> -->
+    <!-- <view>{{filePath2}}</view> -->
+    <view>{{fileContent}}</view>
   </div>
 </template>
 
@@ -25,7 +29,9 @@ export default {
   data () {
     return {
       fileValue: {},
-
+      filePath: '',
+      filePath2: '',
+      fileContent: ''
     }
   },
 
@@ -34,14 +40,48 @@ export default {
   methods: {
     select(e) {
       console.log(e);
-      // console.log(this.fileValue);
-      const readerFile = new FileReader()
-      readerFile.readAsText(e.tempFiles[0].file);
-      // 文件内容
-      const fileContent = readerFile.result
-      console.log(readerFile);
+      const _this = this
+		  /* #ifdef APP-ANDROID */
+        this.filePath = e.tempFilePaths
+        this.filePath2 = e.tempFiles[0].cloudPath
+        // console.log(this.fileValue);
+        const readerFile = new FileReader()
+        readerFile.readAsText(e.tempFiles[0].file);
+        // 文件内容
+        const fileContent = readerFile.result
+        console.log(readerFile);
+      /* #endif */
+		  /* #ifdef MP-WEIXIN */
+        const fs = wx.getFileSystemManager()
+        const fd = fs.openSync({filePath: e.tempFiles[0].path})
+        fs.readFile({
+          filePath: e.tempFiles[0].path,
+          encoding: 'utf8',
+          // position: 0,
+          // length: 100,
+          success(res) {
+            // fs.fstat({
+            //   fd,
+            //   success(res) {
+            //     console.log(res)
+            //   }
+            // })
+            _this.fileContent = res.data
+            console.log(res);
+          }
+        })
+      /* #endif */
     },
-
+    selectFile() {
+      wx.chooseMessageFile({
+        count: 1,
+        type: 'file',
+        extension: ['pdf','txt','word'],
+        success: (res) => {
+          this.select(res);
+        }
+      })
+    }
   },
 
   created() {}
